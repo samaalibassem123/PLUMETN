0c5ee95a3a660b66d35accea3801e370412eba52
samaalibassem123 <83089177+samaalibassem123@users.noreply.github.com>
2024-01-19 13:01:44 +0100

    Add files via upload

--- app/Etudiant/page.jsx
+++ app/Etudiant/page.jsx
@@ -1,8 +1,9 @@
+
 import RegisterForm from '@/components/RegisterForm'
 import React from 'react'
 
 export default function etudiant() {
   return (
-  <RegisterForm/>
+  <RegisterForm ok={true} />
   )
 }
--- app/professeur/page.jsx
+++ app/professeur/page.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import RegisterForm from '@/components/RegisterForm'
 
 export default function professeur() {
   return (
-    <div>professeur</div>
+    <RegisterForm ok={false} />
   )
 }
