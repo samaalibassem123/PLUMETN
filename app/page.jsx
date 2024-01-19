655d83e54b3f1b16307b9c0e6237672cd61c007a
samaalibassem123 <83089177+samaalibassem123@users.noreply.github.com>
2024-01-19 14:14:05 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,9 @@ import Link from "next/link"
 
 export default function page() {
   return (
-      <form className="font-ft font-bold shadow-md bg-gra-500 p-10 md:h-1/2 md:w-1/2 absolute w-5/6 h-1/2 bg-white space-y-6 select-none">
+      <form className="font-ft font-bold shadow-md bg-gra-500 p-10 md:h-1/2 md:w-1/2 absolute w-5/6 h-1/2 bg-white space-y-7 select-none">
         <h1 className=" text-3xl select-none">La Plume</h1>
-        <hr className="bg-green-500 h-0.5 select-none m-2"/>
+        <hr className="bg-green-500 h-0.5 select-none "/>
         <div className="flex flex-col justify-center h-1/2" >
           <Link href='/Etudiant' className="select-none m-3 cursor-pointer border border-gray-900 p-5 hover:bg-black hover:text-white"><p className="select-none">Etudiant</p></Link>
           <Link href='/professeur' className="select-none m-3 cursor-pointer border border-gray-900 p-5 hover:bg-black hover:text-white">Professeur</Link>
